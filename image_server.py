e9fb6a9a8d3cd6162c0901834a3ad3d14bb5b8dd
mikey-boy <michael.argany@protonmail.com>
2023-11-01 13:35:57 -0400

    Configure black formatter

--- image_server.py
+++ image_server.py
@@ -1,7 +1,9 @@
+import logging
 import os
 import uuid
-import logging
+
 from werkzeug.datastructures import FileStorage
+
 
 class ImageServer:
     def __init__(self, image_folder: str) -> None:
@@ -11,23 +13,22 @@ class ImageServer:
     def store_image(self, image: FileStorage) -> dict:
         filename_on_server = str(uuid.uuid4())
         image.save(os.path.join(self.image_folder, filename_on_server))
-        return {'filename': image.filename, 'filenameOnServer': filename_on_server}
+        return {"filename": image.filename, "filenameOnServer": filename_on_server}
 
     def store_images(self, images: list[FileStorage]) -> list[dict]:
         files = []
         for image in images:
-            if image.filename != '':
+            if image.filename != "":
                 files.append(self.store_image(image))
         return files
-    
+
     def remove_image(self, image: str):
         full_path = os.path.join(self.image_folder, image)
         try:
-            os.remove(full_path) 
+            os.remove(full_path)
         except FileNotFoundError:
             logging.warning(f"Image not found on server: '{full_path}'")
 
-
     def remove_images(self, images: list[dict]):
         for image in images:
-            self.remove_image(image['filenameOnServer'])
+            self.remove_image(image["filenameOnServer"])
--- utils.py
+++ utils.py
@@ -3,13 +3,14 @@ def availability_to_int(a: list[str]) -> int:
     for i in range(7):
         for j in range(3):
             if f"{i}-{j}" in a:
-                availability += 2 ** (i*3 + j)
+                availability += 2 ** (i * 3 + j)
     return availability
+
 
 def availability_to_list(a: int) -> list[str]:
     availability = []
     for i in range(7):
         for j in range(3):
-            if a & (1 << (i*3 + j)) != 0:
+            if a & (1 << (i * 3 + j)) != 0:
                 availability.append(f"{i}-{j}")
     return availability

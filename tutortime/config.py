e5153bafda04cd18eed72874f4017546f33a0cb5
mikey-boy <michael.argany@protonmail.com>
2024-02-20 20:26:05 -0500

    Configure Google and Facebook Oauth providers

--- tutortime/config.py
+++ tutortime/config.py
@@ -6,3 +6,11 @@ class DevelopmentConfig(Config):
     SECRET_KEY = "racecar"
     IMAGE_FOLDER = "static/uploads"
     SQLALCHEMY_DATABASE_URI = "sqlite:///project.db"
+    FACEBOOK_OAUTH = {
+        "client_id": "",
+        "client_secret": "",
+    }
+    GOOGLE_OAUTH = {
+        "client_id": "",
+        "client_secret": "",
+    }

3aa015e9e30f7843a939117ab6fa74f33f5f366f
mikey-boy <michael.argany@protonmail.com>
2024-02-26 19:26:00 -0500

    Configure app to run on render cloud

--- tutortime/config-sample.py
+++ tutortime/config-sample.py
@@ -1,3 +1,6 @@
+import os
+
+
 class Config(object):
     pass
 
@@ -6,12 +9,12 @@ class DevelopmentConfig(Config):
     # --------- CHANGE ME --------- #
     SECRET_KEY = "racecar"
     FACEBOOK_OAUTH = {
-        "client_id": "",
-        "client_secret": "",
+        "client_id": os.environ["FACEBOOK_CLIENT_ID"],
+        "client_secret": os.environ["FACEBOOK_CLIENT_SECRET"],
     }
     GOOGLE_OAUTH = {
-        "client_id": "",
-        "client_secret": "",
+        "client_id": os.environ["GOOGLE_CLIENT_ID"],
+        "client_secret": os.environ["GOOGLE_CLIENT_SECRET"],
     }
     # ----------------------------- #
 

4bf1fc86b43732a3fce9f81c646d7cefd768745e
mikey-boy <michael.argany@protonmail.com>
2024-03-20 10:01:41 -0400

    Provide empty strings for Oauth config in local development mode

--- tutortime/config-sample.py
+++ tutortime/config-sample.py
@@ -8,8 +8,18 @@ class Config(object):
 class LocalDevelopmentConfig(Config):
     SECRET_KEY = "racecar"
     DATA_FOLDER = "static/"
-    IMAGE_FOLDER = os.path.join(DATA_FOLDER, "uploaded_images")
+    IMAGE_FOLDER = os.path.join(DATA_FOLDER, "uploads")
     SQLALCHEMY_DATABASE_URI = "sqlite:///tutortime.db"
+
+    # OAuth integration won't work but local acccounts should
+    FACEBOOK_OAUTH = {
+        "client_id": "",
+        "client_secret": "",
+    }
+    GOOGLE_OAUTH = {
+        "client_id": "",
+        "client_secret": "",
+    }
 
 
 class CloudDevelopmentConfig(Config):
@@ -18,7 +28,7 @@ class CloudDevelopmentConfig(Config):
 
     # Store application data on a persistent disk in Render
     DATA_FOLDER = "/var/lib/data/tutortime/"
-    IMAGE_FOLDER = os.path.join(DATA_FOLDER, "uploaded_images")
+    IMAGE_FOLDER = os.path.join(DATA_FOLDER, "uploads")
     SQLALCHEMY_DATABASE_URI = "sqlite:////var/lib/data/tutortime/tutortime.db"
 
     # OAuth2 integration with various providers
